70e78566b704b962636059c9ddf0f3733a751271
David Clarance <dclarance@gmail.com>
2015-02-22 13:12:15 +0530

    r-progassign2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions calculate and store/cache inverses of matrices. 
+## Once calculated, the inverse doesn't need to be recalculated, it will be retrieved from memory
 
-## Write a short comment describing this function
+## This function creates a list containing four functions to set and get the value of the vector and to set and get the value of the inverse     
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y #sets x to y for the whole function
+    m <<- NULL
+  }
+  get <- function() x #report the value of x in its lexical scope
+  setinverse <- function(solve) m <<- solve #works in tandem with the cacheSolve function
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)  #creates list of functions defined above
+}   
 
-}
 
-
-## Write a short comment describing this function
+## Returns to inverse of a matrix, if it has already been calculated the inverse is retrieved from the cache and computation is skipped
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached inverse") #returns stored inverse if it has been computed earlier
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...) #calculates inverse if it hasn't been computed earlier
+  x$setinverse(m)
+  m
 }
